1ca0ba5c72e46e71d80fb2c181f7eedf1d74105e
Pdahiya5033 <parteek_12015033@nitkkr.ac.in>
2022-02-01 10:04:29 -0800

    fixed bug for not showing songs list

--- app/src/main/java/com/example/musicplayer_ui/MainActivity.java
+++ app/src/main/java/com/example/musicplayer_ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.musicplayer_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,6 +15,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -35,43 +38,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    RecyclerView recyclerView;
-    SongsAdapter songsAdapter;
-//    String[] items;
-
+    private static final String TAG="Main Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.recyclerView);
-        storagePermission();
+//        recyclerView = findViewById(R.id.recyclerView);
+        if (savedInstanceState == null) {
+            FragmentManager fm = getSupportFragmentManager();
+            fm.beginTransaction().setReorderingAllowed(true)
+                    .add(R.id.fragment_holder, RecyclerViewFragment.class, null)
+                    .addToBackStack(null)
+                    .commit();
+        }
+
+        //storagePermission();
     }
-
-    public void storagePermission(){
-        Dexter.withContext(this)
-                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                .withListener(new PermissionListener() {
-                    @Override
-                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                        // runtime permission given to store songs from external storage to app
-//                        displaySongs();
-                        fetchSongs();
-                    }
-
-                    @Override
-                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-
-                    }
-
-                    @Override
-                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-                        permissionToken.continuePermissionRequest();
-
-                    }
-                })
-                .check();
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"inside in destroy of main");
     }
+}
+
+
     // returns mp3 file from a folder
 //    public ArrayList<File> fetchMusic(File file) {
 //        ArrayList<File> arrayList = new ArrayList<>();
@@ -130,74 +120,3 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //    }
 
-    private void fetchSongs(){
-        // define list to carry the songs
-        List<Songs> songs = new ArrayList<>();
-        Uri songLibraryUri;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
-            songLibraryUri = MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL);
-        }else {
-            songLibraryUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        }
-
-        // projection : what do i need to select from media store, because media store is a collection of database/tables which have media items in user's device
-        // basically what i need from user's device
-        String[] projection = new String[]{
-                MediaStore.Audio.Media._ID,
-                MediaStore.Audio.Media.DISPLAY_NAME, // song name
-                MediaStore.Audio.Media.DURATION, // song duration
-                MediaStore.Audio.Media.ALBUM_ID, // song image
-        };
-
-        // sort order of songs
-        String sortOrderOfSongsLists = MediaStore.Audio.Media.DATE_ADDED + " DESC ";  // sorting date wise in descending order
-
-        //Querying
-        try(Cursor cursor = getContentResolver().query(songLibraryUri, projection, null, null, sortOrderOfSongsLists)) {
-
-            //cache the cursor indices
-            int idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID);  // index of the cursor
-            int nameColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME);
-            int durationColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION);
-            int albumIdColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM_ID);
-
-            //getting the values from cursor indices
-            while (cursor.moveToNext()) {
-                //get values of columns for a give audio file
-                long id = cursor.getLong(idColumn);
-                String name = cursor.getString(nameColumn);
-                int duration = cursor.getInt(durationColumn);
-                long albumId = cursor.getLong(albumIdColumn);
-
-                //song uri
-                Uri uri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id);
-
-                //album art uri
-                Uri albumArtUri = ContentUris.withAppendedId(Uri.parse("content://media/external/audio/albumArt"), albumId);
-
-                //remove .mp3 extension from song name
-                name = name.substring(0, name.lastIndexOf("."));
-
-                // get songs item from model class
-                Songs song = new Songs(id, uri, name, duration, albumId, albumArtUri);
-                // add songs to song Arraylist
-                songs.add(song);
-            }
-            // display songs in recyclerView
-            displaySongs(songs);
-            // Number of songs available
-            Toast.makeText(getApplicationContext(), "Number Of Songs " +songs.size(), Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
-    private void displaySongs(List<Songs> songs) {
-        // layout manager
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        // adapter
-        songsAdapter = new SongsAdapter(songs);
-        recyclerView.setAdapter(songsAdapter);
-    }
-}
